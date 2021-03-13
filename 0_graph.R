9136b680697c269f82373de7c88d3c60354bc08f
Nakhirot0327 <32269892+Nakhirot0327@users.noreply.github.com>
2021-03-13 17:39:34 +0800

    Delete 0_graph.R

--- 0_graph.R
+++
@@ -1,7 +0,0 @@
-x <- seq(-2,2,by=0.01)
-plot(x,x^2,type="l",ylab="f(x)")
-
-plot(x,-x^2,type="l",ylab="f(x)")
-plot(x,exp(-x^2/2),type="l",ylab="f(x)")
-plot(x,1/sqrt(2*pi)*exp(-x^2/2),type="l",ylab="f(x)")
-plot(x,1/sqrt(2*pi)*exp(-x^2/2),type="l",ylab="f(x)")
